32d4c2388e8f6b956e0fa49d577a5afa3c5aada6
David Moller <davidmollera@hotmail.com>
2023-06-01 12:17:49 -0500

    created a nav css file and added the css for the nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,11 +2,12 @@ import { Menu } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
 import React from 'react';
 import logo from '../Assets/wfg-logo.webp'
+import '../Css/Nav.css'
 
 const Nav = () => {
     return (
         <nav>
-            <figure className='logo__wrapper'>
+            <figure className='nav__logo--wrapper'>
                 <img src={logo} alt="" />
             </figure>
             <IconButton>
