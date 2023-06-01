3619adc9fd6690eda584f2cc2ded75908dfae5bc
David Moller <davidmollera@hotmail.com>
2023-06-01 13:34:16 -0500

    advanced the home page and info container along with the corresponding css

--- src/Components/InfoContainer.jsx
+++ src/Components/InfoContainer.jsx
@@ -1,14 +1,21 @@
+import { Mail } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import React from "react";
+import profile from '../Assets/profile.jpg'
 
 const InfoContainer = () => {
   return (
     <div className="info__container">
       <h2 className="info__title">Hello! My name is Marcel 👋</h2>
       <figure className="info__img--wrapper">
-        <img src="" alt="" className="info__img" />
+        <img src={profile} alt="" className="info__img" />
       </figure>
       <ul className="info__list">
-        <li className="info__list--item"></li>
+        <li className="info__list--item">
+            <IconButton>
+                <Mail/>
+            </IconButton>
+        </li>
       </ul>
     </div>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import InfoContainer from '../Components/InfoContainer';
+import '../Css/Home.css'
 
 const Home = () => {
     return (
         <section id="landing">
             <div className="container landing__container">
                 <div className="row landing__row">
-
+                    <InfoContainer/>
                 </div>
             </div>
         </section>
