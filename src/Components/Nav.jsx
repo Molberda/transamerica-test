da23e115bb6772674719113be62e02e47f868b31
David Moller <davidmollera@hotmail.com>
2023-06-01 10:48:05 -0500

    imported the nav component to the app jsx and added initial html for the nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,9 +1,13 @@
+import { IconButton } from '@mui/material';
 import React from 'react';
 
 const Nav = () => {
     return (
         <nav>
-            
+            <figure className='logo__wrapper'>
+                <img src="" alt="" />
+            </figure>
+            <IconButton></IconButton>
         </nav>
    );
 }
