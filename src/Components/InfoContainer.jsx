8558bc856e3425ce6bd21cb117c28913b663c532
David Moller <davidmollera@hotmail.com>
2023-06-02 17:25:18 -0500

    created a highlight span class and gave it css

--- src/Components/InfoContainer.jsx
+++ src/Components/InfoContainer.jsx
@@ -6,7 +6,7 @@ import profile from '../Assets/profile.jpg'
 const InfoContainer = () => {
   return (
     <div className="info__container">
-      <h2 className="info__title">Hello! <br /> My name is Marcel</h2>
+      <h2 className="info__title">Hello! <br /> My name is <span className="highlight">Marcel</span></h2>
       <figure className="info__img--wrapper">
         <img src={profile} alt="" className="info__img" />
       </figure>
