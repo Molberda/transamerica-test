9d26de1cb4a4525a8f8f4a645a41c998910854d8
David Moller <davidmollera@hotmail.com>
2023-06-02 12:16:37 -0500

    moved the generic container and row classes to the app css file and added a gap to the elements in info container

--- src/Components/InfoContainer.jsx
+++ src/Components/InfoContainer.jsx
@@ -6,7 +6,7 @@ import profile from '../Assets/profile.jpg'
 const InfoContainer = () => {
   return (
     <div className="info__container">
-      <h2 className="info__title">Hello! My name is Marcel 👋</h2>
+      <h2 className="info__title">Hello! My name is Marcel</h2>
       <figure className="info__img--wrapper">
         <img src={profile} alt="" className="info__img" />
       </figure>
