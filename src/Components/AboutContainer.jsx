8d0bef0a55a68f9bb44e96e915f11b18eba24d4e
David Moller <davidmollera@hotmail.com>
2023-06-02 13:27:14 -0500

    added the css for the about container elements

--- src/Components/AboutContainer.jsx
+++ src/Components/AboutContainer.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const AboutContainer = () => {
     return (
         <div className='about__container'>
-            <p className="about__text">testing</p>
+            <p className="about__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium quas accusamus hic. Sint ab dicta pariatur sequi vel repudiandae culpa? Earum error nobis reprehenderit possimus consequatur, id dicta reiciendis perferendis?</p>
             <ExpandMore/>
         </div>
     );
