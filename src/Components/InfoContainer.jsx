fb1733297ce82e747dadf8b349bdac421d79056b
David Moller <davidmollera@hotmail.com>
2023-06-02 16:02:19 -0500

    added df to the landing row and made other minor changes to the css in home page

--- src/Components/InfoContainer.jsx
+++ src/Components/InfoContainer.jsx
@@ -6,11 +6,26 @@ import profile from '../Assets/profile.jpg'
 const InfoContainer = () => {
   return (
     <div className="info__container">
-      <h2 className="info__title">Hello! My name is Marcel</h2>
+      <h2 className="info__title">Hello! <br /> My name is Marcel</h2>
       <figure className="info__img--wrapper">
         <img src={profile} alt="" className="info__img" />
       </figure>
       <ul className="info__list">
+        <li className="info__list--item">
+            <IconButton>
+                <Mail/>
+            </IconButton>
+        </li>
+        <li className="info__list--item">
+            <IconButton>
+                <Mail/>
+            </IconButton>
+        </li>
+        <li className="info__list--item">
+            <IconButton>
+                <Mail/>
+            </IconButton>
+        </li>
         <li className="info__list--item">
             <IconButton>
                 <Mail/>
