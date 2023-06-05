b841e9b6b2480c39a86343cc78d9c3aa100524b8
David Moller <davidmollera@hotmail.com>
2023-06-05 13:02:39 -0500

    added html to the product component

---
+++ src/Sections/Product.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <section id="product">
+      <div className="container product__container">
+        <div className="row product__row">
+          <div className="product__organizer">
+            <div className="product">
+              <figure className="product__wrapper">
+                <img src="" alt="" className="product__img" />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Product;
