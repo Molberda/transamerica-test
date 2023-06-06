a66c86a82b09770120ee6fa7da3b71bec13cefc3
David Moller <davidmollera@hotmail.com>
2023-06-06 11:58:44 -0500

    added the link and button for all products after the product map in products file

--- src/Sections/Products.jsx
+++ src/Sections/Products.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ProductMap from "../Components/ProductMap";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
 
 const Products = ({ products }) => {
   return (
@@ -10,6 +12,11 @@ const Products = ({ products }) => {
             {products.map((item) => (
               <ProductMap products={products} item={item} key={item.id} />
             ))}
+            <Link>
+              <Button>
+                <h3>View All Products</h3>
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
