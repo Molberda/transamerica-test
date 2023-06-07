cca86f6752965392850d53c08a29931a16826814
David Moller <davidmollera@hotmail.com>
2023-06-07 12:40:55 -0500

    created the rating component and added the function so it transforms a number rating into the star icon and star halfs

---
+++ src/Components/Rating.jsx
@@ -0,0 +1,15 @@
+import { Star, StarHalf } from "@mui/icons-material";
+import React from "react";
+
+const Rating = ({ rating }) => {
+  return (
+    <div>
+      {new Array(Math.floor(rating)).fill(0).map((_, index) => (
+        <Star key={index} />
+      ))}
+      {!Number.isInteger(rating) && <StarHalf />}
+    </div>
+  );
+};
+
+export default Rating;
--- src/Components/ReviewMap.jsx
+++ src/Components/ReviewMap.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Rating from './Rating';
 
 const ReviewMap = ({ item }) => {
     return (
         <div className="review">
             <h3 className="review__name">{item.name}</h3>
-            
+            <p className="review__description">{item.description}</p>
+            <Rating rating={item.rating}/>
         </div>
     );
 }
