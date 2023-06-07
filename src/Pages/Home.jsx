8d6fe4c369c356e56cc9d392945be48618e6b384
David Moller <davidmollera@hotmail.com>
2023-06-07 10:17:34 -0500

    created a review section component and imported it to the home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Landing from '../Sections/Landing';
 import '../Css/Home.css'
 import Products from '../Sections/Products';
+import Reviews from '../Sections/Reviews';
 
 const Home = ({ products }) => {
     return (
         <main id="home">
             <Landing/>
             <Products products={products}/>
+            <Reviews/>
         </main>
     );
 }
