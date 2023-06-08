9d34bd6a7fd2089d07b051a9a2d3bc0b8bb975d4
David Moller <davidmollera@hotmail.com>
2023-06-08 08:52:35 -0500

    added html for the review map component and added more css for the review map elements

--- src/Components/ReviewMap.jsx
+++ src/Components/ReviewMap.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import Rating from './Rating';
+import React from "react";
+import Rating from "./Rating";
 
 const ReviewMap = ({ item }) => {
-    return (
-        <div className="review">
-            <h3 className="review__name">{item.name}</h3>
-            <p className="review__description">{item.description}</p>
-            <Rating rating={item.rating}/>
-        </div>
-    );
-}
+  return (
+    <div className="review">
+      <div className="review__organizer">
+        <h3 className="review__name">{item.name}</h3>
+        <Rating rating={item.rating} />
+        <p className="review__description">{item.description}</p>
+      </div>
+      <figure className="review__img--wrapper">
+        <img src={item.img} alt="" className="review__img" />
+      </figure>
+    </div>
+  );
+};
 
 export default ReviewMap;
--- src/Reviews.js
+++ src/Reviews.js
@@ -1,8 +1,9 @@
 export const reviews = [
     {
         id: 1,
-        name: "monica",
+        name: "Monica",
         rating: 4.5,
-        description: 'description test'
+        description: 'Description test',
+        img: 'https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png'
     },
 ]
