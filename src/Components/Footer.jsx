fdb904060c69f67a525afece81a641bf353e71d5
David Moller <davidmollera@hotmail.com>
2023-06-08 10:52:33 -0500

    created the css file for the footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../Css/Footer.css'
 
 const Footer = () => {
   return (
@@ -12,6 +13,19 @@ const Footer = () => {
           <ul className="footer__list">
             <li className="footer__list--item">
               <Link>Products</Link>
+              <div className="hover__effect"></div>
+            </li>
+            <li className="footer__list--item">
+              <Link>Contact</Link>
+              <div className="hover__effect"></div>
+            </li>
+            <li className="footer__list--item">
+              <Link>More about us</Link>
+              <div className="hover__effect"></div>
+            </li>
+            <li className="footer__list--item">
+              <Link>Products</Link>
+              <div className="hover__effect"></div>
             </li>
           </ul>
         </div>
