1c0a54aed94be7c6c73e931dae009b17c53c5a82
David Moller <davidmollera@hotmail.com>
2023-06-08 10:33:30 -0500

    added html to the footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div>
-            
+  return (
+    <footer>
+      <div className="container footer__container">
+        <div className="row footer__row">
+          <figure className="footer__logo--wrapper">
+            <img src="" alt="" className="footer__logo" />
+          </figure>
+          <ul className="footer__list">
+            <li className="footer__list--item">
+              <Link>Products</Link>
+            </li>
+          </ul>
         </div>
-    );
-}
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
