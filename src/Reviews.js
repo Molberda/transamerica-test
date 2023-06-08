010c8101ca283a0c11337a0061ead9c1d8690135
David Moller <davidmollera@hotmail.com>
2023-06-08 10:15:18 -0500

    added df to the reviews organizer and a gap to separate the reviews from one another

--- src/Reviews.js
+++ src/Reviews.js
@@ -6,4 +6,11 @@ export const reviews = [
         description: 'Description test',
         img: 'https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png'
     },
+    {
+        id: 2,
+        name: "Monica",
+        rating: 4.5,
+        description: 'Description test',
+        img: 'https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png'
+    },
 ]
