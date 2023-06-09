32a2960f79bc5c5f06cea2159d13c9588be173c4
David Moller <davidmollera@hotmail.com>
2023-06-09 13:31:31 -0500

    added css and more html for the burger component

--- src/Components/Burger.jsx
+++ src/Components/Burger.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../Css/Burger.css'
 
 const Burger = () => {
     return (
-        <section id="landing" className="burger">
+        <section id="burger">
             <div className="container burger__container">
                 <div className="row burger__row">
                     <ul className="burger__list">
-                        <li>
-                            <Link to='/'>Home</Link>
+                        <li className='burger__list--item'>
+                            <Link to='/'><h2>Home</h2></Link>
+                        </li>
+                        <li className='burger__list--item'>
+                            <Link to='/'><h2>Home</h2></Link>
+                        </li>
+                        <li className='burger__list--item'>
+                            <Link to='/'><h2>Home</h2></Link>
+                        </li>
+                        <li className='burger__list--item'>
+                            <Link to='/'><h2>Home</h2></Link>
                         </li>
                     </ul>
                 </div>
