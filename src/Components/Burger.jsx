2ef76fce3239959a012caf55ccbe3abc5fb93e32
David Moller <davidmollera@hotmail.com>
2023-06-09 12:54:21 -0500

    added the basic html for the burger component

--- src/Components/Burger.jsx
+++ src/Components/Burger.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Burger = () => {
     return (
-        <div>
-            
-        </div>
+        <section id="landing" className="burger">
+            <div className="container burger__container">
+                <div className="row burger__row">
+                    <ul className="burger__list">
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
     );
 }
 
