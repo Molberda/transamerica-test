ea10a4e09be96116cab3b263ef9f0e229c4127f5
David Moller <davidmollera@hotmail.com>
2023-06-12 10:22:40 -0500

    added basic html to the productlist component

--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const ProductList = () => {
     return (
-        <div>
-            
-        </div>
+        <section id="productlist">
+            <div className="container productlist__container">
+                <div className="row productlist__row">
+                    <h1 className="productlist__title">All Our Products</h1>
+                    
+                </div>
+            </div>
+        </section>
     );
 }
 
