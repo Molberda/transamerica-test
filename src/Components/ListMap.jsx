65408b0756306bce6d9bc0f1bd9240e203e2474f
David Moller <davidmollera@hotmail.com>
2023-06-12 11:32:07 -0500

    added an icon button and link icon to the listmap items and added basic css for the productlist page

--- src/Components/ListMap.jsx
+++ src/Components/ListMap.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import { InsertLink } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+import React from "react";
 
-const ListMap = ({item}) => {
-    return (
-        <div className='productlist__item'>
-            <h3 className="productlist__name">{item.name}</h3>
-            <h3 className="productlist__type">{item.type}</h3>
-        </div>
-    );
-}
+const ListMap = ({ item }) => {
+  return (
+    <div className="productlist__item">
+      <h3 className="productlist__name">{item.name}</h3>
+      <h3 className="productlist__type">{item.type}</h3>
+      <IconButton>
+        <InsertLink />
+      </IconButton>
+    </div>
+  );
+};
 
 export default ListMap;
