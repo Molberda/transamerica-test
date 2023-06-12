99b8ebefe95e28c4acc1c3219d6eae990d3f4392
David Moller <davidmollera@hotmail.com>
2023-06-12 10:32:37 -0500

    created and imported the css file for the productlist page

--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../Css/ProductList.css'
 
 const ProductList = () => {
     return (
@@ -6,7 +7,7 @@ const ProductList = () => {
             <div className="container productlist__container">
                 <div className="row productlist__row">
                     <h1 className="productlist__title">All Our Products</h1>
-                    
+                    <div className="productlist__organizer"></div>
                 </div>
             </div>
         </section>
