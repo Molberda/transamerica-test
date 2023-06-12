807e9324851ae9d1dacd467ddaa00195d009b02f
David Moller <davidmollera@hotmail.com>
2023-06-12 09:44:21 -0500

    created the html for the burger component

--- src/Components/Burger.jsx
+++ src/Components/Burger.jsx
@@ -1,30 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../Css/Burger.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../Css/Burger.css";
+import { Button } from "@mui/material";
 
 const Burger = () => {
-    return (
-        <section id="burger">
-            <div className="container burger__container">
-                <div className="row burger__row">
-                    <ul className="burger__list">
-                        <li className='burger__list--item'>
-                            <Link to='/'><h2>Home</h2></Link>
-                        </li>
-                        <li className='burger__list--item'>
-                            <Link to='/'><h2>Home</h2></Link>
-                        </li>
-                        <li className='burger__list--item'>
-                            <Link to='/'><h2>Home</h2></Link>
-                        </li>
-                        <li className='burger__list--item'>
-                            <Link to='/'><h2>Home</h2></Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section id="burger">
+      <div className="container burger__container">
+        <div className="row burger__row">
+          <ul className="burger__list">
+            <li className="burger__list--item">
+              <Button>
+                <Link to="/">
+                  <h2>Home</h2>
+                </Link>
+              </Button>
+            </li>
+            <li className="burger__list--item">
+              <Button>
+                <Link to="/">
+                  <h2>Home</h2>
+                </Link>
+              </Button>
+            </li>
+            <li className="burger__list--item">
+              <Button>
+                <Link to="/">
+                  <h2>Home</h2>
+                </Link>
+              </Button>
+            </li>
+            <li className="burger__list--item">
+              <Button>
+                <Link to="/">
+                  <h2>Home</h2>
+                </Link>
+              </Button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Burger;
