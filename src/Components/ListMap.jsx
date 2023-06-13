673d8c3a0b24c386867f0bc24f421aa0997304b4
David Moller <davidmollera@hotmail.com>
2023-06-13 10:20:52 -0500

    added a div that contains both the name and type of product in the productlist page

--- src/Components/ListMap.jsx
+++ src/Components/ListMap.jsx
@@ -5,8 +5,10 @@ import React from "react";
 const ListMap = ({ item }) => {
   return (
     <div className="productlist__item">
-      <h3 className="productlist__name">{item.name}</h3>
-      <h3 className="productlist__type">{item.type}</h3>
+      <div className="productlist__info--container">
+        <h3 className="productlist__name">{item.name}</h3>
+        <h3 className="productlist__type">{item.type}</h3>
+      </div>
       <IconButton>
         <InsertLink />
       </IconButton>
