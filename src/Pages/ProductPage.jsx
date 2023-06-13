74c8f87923c1f71c001b22fab4957e58eb42e0d5
David Moller <davidmollera@hotmail.com>
2023-06-13 11:21:34 -0500

    created a productpage css file and imported it to the jsx file, also added basic html to the jsx file

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import '../Css/Productpage.css'
 
 const ProductPage = () => {
     return (
-        <div>
-            
-        </div>
+        <section id="productpage">
+            <div className="container productpage__container top__section">
+                <div className="row productpage__row">
+
+                </div>
+            </div>
+        </section>
     );
 }
 
