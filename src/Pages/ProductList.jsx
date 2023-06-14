f86e460212fc80f8ceed9696c27737feb79a2ea8
David Moller <davidmollera@hotmail.com>
2023-06-14 11:26:20 -0500

    added a default option for the filter and added a working investment option

--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -5,20 +5,26 @@ import { useState } from "react";
 
 const ProductList = ({ products: initialproducts }) => {
 
-    const [ products, setProducts ] = useState(initialproducts);
-    const insurance = initialproducts.filter((item) => item.type === 'insurance')
-    function filterItems(filter){
-        if (filter === 'INSURANCE'){
-            setProducts(insurance)
-        }
+  const [products, setProducts] = useState(initialproducts);
+  const insurance = initialproducts.filter((item) => item.type === "insurance");
+  const investment = initialproducts.filter((item) => item.type === 'investment')
+
+  function filterItems(filter) {
+    if (filter === "INSURANCE") {
+      setProducts(insurance);
     }
+    if (filter === "INVESTMENT") {
+      setProducts(investment);
+    }
+  }
 
   return (
     <section id="productlist">
       <div className="container productlist__container top__section">
         <div className="row productlist__row">
           <h1 className="productlist__title">All Our Products</h1>
-          <select id="filter" onChange={(e) => filterItems(e.target.value)}>
+          <select id="filter" defaultValue='DEFAULT' onChange={(e) => filterItems(e.target.value)}>
+            <option value="DEFAULT">Filter</option>
             <option value="INSURANCE">Insurance</option>
             <option value="INVESTMENT">Investment</option>
           </select>
