eeec6ccd9d1fb35330f6b1d73fcbb6d45fa6dd7a
David Moller <davidmollera@hotmail.com>
2023-06-14 10:46:10 -0500

    added select tag and options to the productlist file

--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import '../Css/Productlist.css'
-import ListMap from '../Components/ListMap';
+import React from "react";
+import "../Css/Productlist.css";
+import ListMap from "../Components/ListMap";
 
 const ProductList = ({ products }) => {
-    return (
-        <section id="productlist">
-            <div className="container productlist__container top__section">
-                <div className="row productlist__row">
-                    <h1 className="productlist__title">All Our Products</h1>
-                    <div className="productlist__organizer">
-                        {products.map((item) => <ListMap key={item.id} item={item}/> )}
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section id="productlist">
+      <div className="container productlist__container top__section">
+        <div className="row productlist__row">
+          <h1 className="productlist__title">All Our Products</h1>
+          <select id="filter">
+            <option value="INSURANCE">Insurance</option>
+            <option value="INVESTMENT">Investment</option>
+          </select>
+          <div className="productlist__organizer">
+            {products.map((item) => (
+              <ListMap key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default ProductList;
