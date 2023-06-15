d4c1b02fc0884def76d23a9d17649ce19c7a2805
David Moller <davidmollera@hotmail.com>
2023-06-15 10:42:26 -0500

    added html to display product type and product info on the product page aswell as basic css for all of it

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,25 +1,28 @@
-import React, { useEffect } from 'react';
-import '../Css/Productpage.css'
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from "react";
+import "../Css/Productpage.css";
+import { useParams } from "react-router-dom";
 
 const ProductPage = ({ products }) => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0 });
+  }, []);
 
-    useEffect(() => {
-        window.scrollTo({ top: 0, left: 0 })
-    }, []);
+  const { id } = useParams();
+  const product = products.find((product) => product.id === +id);
 
-    const { id } = useParams()
-    const product = products.find((product) => product.id === +id)
-
-    return (
-        <section id="productpage">
-            <div className="container productpage__container top__section">
-                <div className="row productpage__row">
-                    <h2 className="productpage__title">{product.name}</h2>
-                </div>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section id="productpage">
+      <div className="container productpage__container top__section">
+        <div className="row productpage__row">
+          <h2 className="productpage__title">{product.name}</h2>
+          <div className="productpage__organizer">
+            <h4 className="productpage__subtitle">{product.type}</h4>
+            <p className="productpage__text">{product.info}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default ProductPage;
