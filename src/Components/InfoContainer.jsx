111dbe754b08f120e50a6eff5619d326507d16bb
David Moller <davidmollera@hotmail.com>
2023-06-19 18:50:43 -0500

    creaetd anchor tags for each of the info container icons and added their href also imported IUL brochure to the assets folder

--- src/Components/InfoContainer.jsx
+++ src/Components/InfoContainer.jsx
@@ -1,35 +1,45 @@
 import { LinkedIn, Mail, PictureAsPdf, WhatsApp } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
 import React from "react";
-import profile from '../Assets/profile.jpg'
+import profile from "../Assets/profile.jpg";
 
 const InfoContainer = () => {
   return (
     <div className="info__container">
-      <h1 className="info__title section__title">Hello! <br /> My name is <span className="highlight">Marcel</span></h1>
+      <h1 className="info__title section__title">
+        Hello! <br /> My name is <span className="highlight">Marcel</span>
+      </h1>
       <figure className="info__img--wrapper">
         <img src={profile} alt="" className="info__img" />
       </figure>
       <ul className="info__list">
         <li className="info__list--item">
+          <a href="https://wa.me/573163402080" target="_blank" className="info__list--link">
             <IconButton>
-                <WhatsApp/>
+              <WhatsApp />
             </IconButton>
+          </a>
         </li>
         <li className="info__list--item">
+          <a href="mailto:macias.marcel@gmail.com" target="_blank" className="info__list--link">
             <IconButton>
-                <Mail/>
+              <Mail />
             </IconButton>
+          </a>
         </li>
         <li className="info__list--item">
+          <a href="https://www.linkedin.com/feed/" target="_blank" className="info__list--link">
             <IconButton>
-                <LinkedIn/>
+              <LinkedIn />
             </IconButton>
+          </a>
         </li>
         <li className="info__list--item">
+          <a href="" target="_blank" className="info__list--link">
             <IconButton>
-                <PictureAsPdf/>
+              <PictureAsPdf />
             </IconButton>
+          </a>
         </li>
       </ul>
     </div>
