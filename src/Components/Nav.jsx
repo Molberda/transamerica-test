5a2c727b75a77c4ceb85d99b77d97d5b74e81031
David Moller <davidmollera@hotmail.com>
2023-06-20 13:55:22 -0500

    wrapped the nav logo in a link that leads to the home page

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,25 +1,27 @@
-import { Menu } from '@mui/icons-material';
-import { IconButton } from '@mui/material';
-import React from 'react';
-import logo from '../Assets/wfg-logo.webp'
-import '../Css/Nav.css'
+import { Menu } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+import React from "react";
+import logo from "../Assets/wfg-logo.webp";
+import "../Css/Nav.css";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
+  function openMenu() {
+    document.querySelector("body").classList.toggle("menuOpen");
+  }
 
-    function openMenu(){
-        document.querySelector('body').classList.toggle('menuOpen')
-    }
-
-    return (
-        <nav>
-            <figure className='nav__logo--wrapper'>
-                <img src={logo} alt="" />
-            </figure>
-            <IconButton onClick={openMenu}>
-                <Menu/>
-            </IconButton>
-        </nav>
-   );
-}
+  return (
+    <nav>
+      <Link to='/'>
+        <figure className="nav__logo--wrapper">
+          <img src={logo} alt="" />
+        </figure>
+      </Link>
+      <IconButton onClick={openMenu}>
+        <Menu />
+      </IconButton>
+    </nav>
+  );
+};
 
 export default Nav;
