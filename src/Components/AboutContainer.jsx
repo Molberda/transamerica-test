d199f2b2001a96c4dd7f43d9a83043e387b88a31
David Moller <davidmollera@hotmail.com>
2023-06-20 14:08:09 -0500

    increased the size for the landing icons and the view more button in products section

--- src/Components/AboutContainer.jsx
+++ src/Components/AboutContainer.jsx
@@ -6,11 +6,11 @@ const AboutContainer = () => {
     <div className="about__container">
       <p className="about__text">
         Your <span className="highlight">Financial Advisor</span> and{" "}
-        <span className="highlight">Life Insurance Agent</span>. <br /> I'm eager to
-        help new families manage their economic life aswell as provide guidance
-        and safe financial vehicles to <span className="highlight">create</span>{" "}
-        and <span className="highlight">protect</span> your life's work and
-        effort
+        <span className="highlight">Life Insurance Agent</span>. <br /> I'm
+        eager to help new families manage their economic life aswell as provide
+        guidance and safe financial vehicles to{" "}
+        <span className="highlight">create</span> and{" "}
+        <span className="highlight">protect</span> your life's work and effort
       </p>
       <ExpandMore />
     </div>
