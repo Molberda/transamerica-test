cb0fe0366b844ffea6e7d23874d1489664e28999
David Moller <davidmollera@hotmail.com>
2023-06-19 19:18:38 -0500

    added a link tag for the productmap in the productmap component

--- src/Components/ProductMap.jsx
+++ src/Components/ProductMap.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ProductMap = ({ item }) => {
   return (
     <div className="product">
-      <figure className="product__wrapper">
-        <img src={item.img} alt="" className="product__img" />
-        <div className="product__info--container">
-          <h2 className="product__title">{item.name}</h2>
-        </div>
-      </figure>
+      <Link to={`/product/${item.id}`} >
+        <figure className="product__wrapper">
+          <img src={item.img} alt="" className="product__img" />
+          <div className="product__info--container">
+            <h2 className="product__title">{item.name}</h2>
+          </div>
+        </figure>
+      </Link>
     </div>
   );
 };
