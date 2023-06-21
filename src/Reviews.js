228ca7b93b0e50d88a4c0cf729283e966661c82c
David Moller <davidmollera@hotmail.com>
2023-06-21 13:29:52 -0500

    updated the first review description so its not a lorem ipsum text

--- src/Reviews.js
+++ src/Reviews.js
@@ -3,7 +3,7 @@ export const reviews = [
         id: 1,
         name: "Monica",
         rating: 4.5,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis lectus vestibulum, accumsan purus nec, tempor sem. Cras vulputate massa vel porttitor tempus.',
+        description: `I contacted Marcel because he was highly recommended by a close relative. Less than a month from our first meeting I was insured and started saving up for my future. He's always attentive to anything I might need and willing to help when needed.`,
         img: 'https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png'
     },
     {
