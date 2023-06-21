2758d474fded1d1407087ce2e2000deab9da2c0f
David Moller <davidmollera@hotmail.com>
2023-06-21 12:11:39 -0500

    importted the iul brochure to the product page and wrapped the pdf icon with a link that leads to the pdf

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -3,9 +3,9 @@ import "../Css/Productpage.css";
 import { useParams } from "react-router-dom";
 import { IconButton } from "@mui/material";
 import { Mail, PictureAsPdf } from "@mui/icons-material";
+import IUL from "../Assets/Brochure-IUL.pdf";
 
 const ProductPage = ({ products }) => {
-
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0 });
   }, []);
@@ -20,22 +20,24 @@ const ProductPage = ({ products }) => {
           <h1 className="productpage__title section__title">{product.name}</h1>
           <div className="productpage__organizer">
             <h4 className="productpage__subtitle">{product.type}</h4>
-            <p className="productpage__text">{product.info}</p>         
+            <p className="productpage__text">{product.info}</p>
             <ul className="productpage__icon--list">
-            <h4>Extra information:</h4>
+              <h4>Extra information:</h4>
               <li className="productpage__icon">
-                <IconButton>
-                  <PictureAsPdf />
-                </IconButton>
+                <a href={IUL} target="_blank" rel="noreferrer">
+                  <IconButton>
+                    <PictureAsPdf />
+                  </IconButton>
+                </a>
               </li>
               <li className="productpage__icon">
                 <IconButton>
-                  <Mail/>
+                  <Mail />
                 </IconButton>
               </li>
             </ul>
             <figure className="productpage__img--wrapper">
-                <img src={product.img} alt="" className="productpage__img" />
+              <img src={product.img} alt="" className="productpage__img" />
             </figure>
           </div>
         </div>
