98acf621c8118258e4f7d34d6621859b4c068ade
David Moller <davidmollera@hotmail.com>
2023-06-21 13:55:57 -0500

    updated the description for the third review

--- src/Reviews.js
+++ src/Reviews.js
@@ -2,22 +2,22 @@ export const reviews = [
     {
         id: 1,
         name: "Monica",
-        rating: 5,
+        rating: 4.5,
         description: `I contacted Marcel because he was highly recommended by a close relative. Less than a month from our first meeting I was insured and started saving up for my future. He's always attentive to anything I might need and willing to help when needed.`,
         img: 'https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png'
     },
     {
         id: 2,
         name: "James",
-        rating: 4.5,
+        rating: 5,
         description: `Marcel suggested an IUL plan for me and explained me all the benefits this product would have for me and my family, from that point foward the proccess was very straight foward and simple. Now I live happily as an insured man and I can rest easy thinking about my family's future`,
         img: 'https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png'
     },
     {
         id: 3,
-        name: "Monica",
+        name: "Clara",
         rating: 4.5,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis lectus vestibulum, accumsan purus nec, tempor sem. Cras vulputate massa vel porttitor tempus.',
+        description: 'Great service and patient to go around all the different type of products and selecting the best fit for me based on my specific life and accomodations 10/10 would recommend',
         img: 'https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png'
     },
 ]
